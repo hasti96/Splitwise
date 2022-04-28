78607410c187a52207f19a3b6f6e8c9bac493339
Hastimal Paliwal <46267369+hasti96@users.noreply.github.com>
2022-04-28 21:43:10 +0530

    Update ExpenseService.java
    Next session

--- Services/ExpenseService.java
+++ Services/ExpenseService.java
@@ -1,17 +1,19 @@
 package Services;
+
 import Models.*;
 import java.util.*;
 
 class ExpenseService {
-  public static Expense createExpense(ExpenseType expenseType, double amount, User paidBy, List<Split> splits, ExpenseMetadata metadata){
-    switch (expenseType){
+  public static Expense createExpense(ExpenseType expenseType, double amount, User paidBy, List<Split> splits,
+      ExpenseMetadata metadata) {
+    switch (expenseType) {
       case EXACT:
-		return new ExactExpense(amount, paidBy, splits, metadata);
-            
+        return new ExactExpense(amount, paidBy, splits, metadata);
+
       case PERCENT:
-        for(Split split: splits) {
+        for (Split split : splits) {
           PercentSplit percentSplit = (PercentSplit) split;
-          split.setAmount(amount*percentSplit.getPercent()/100);
+          split.setAmount(amount * percentSplit.getPercent() / 100);
         }
         return new PercentExpense(amount, paidBy, splits, metadata);
 
@@ -19,7 +21,7 @@ class ExpenseService {
         int totalNoOfUsers = splits.size();
         double equalAmount = amount / totalNoOfUsers;
 
-        for(Split split: splits) {
+        for (Split split : splits) {
           split.setAmount(equalAmount);
         }
 
@@ -30,3 +32,4 @@ class ExpenseService {
     }
   }
 }
+ss
